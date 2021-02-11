34945d0fa81ce634561b1dd4d1f46eddf57604df
Renato Perez <renato.perez@wekynect.com>
2021-02-10 20:42:10 -0600

    Make csv parsing function type safe

--- src/utils/importCsvFile.ts
+++ src/utils/importCsvFile.ts
@@ -2,13 +2,14 @@ import { join as pathJoin } from 'path';
 import papa from 'papaparse';
 import { read } from './file';
 import { exportJsonFile } from './exportJsonFile';
+import { DeptoPoblacion } from '../lib';
 
 // Helper function to import CSV file.
 
-export const importCsvFile = async (filePath: string): Promise<unknown[]> => {
+export const importCsvFile = async (filePath: string): Promise<DeptoPoblacion[]> => {
   try {
     const textFileData = await read(pathJoin(__dirname, filePath));
-    const result = papa.parse(textFileData, { header: true, dynamicTyping: true });
+    const result = papa.parse<DeptoPoblacion>(textFileData, { header: true, dynamicTyping: true });
     return result.data;
   } catch (error) {
     throw new Error(`Error parsing csv file: ${error?.message ?? error}`);
